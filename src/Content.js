e6b019ad8d2353c1b3d169724157316713451fb0
JMotDQS <jmotlagh@dqstaff.com>
2024-07-01 16:48:02 -0400

    Update Content.js

--- src/Content.js
+++ src/Content.js
@@ -1,18 +1,36 @@
 import React from 'react'
+import {useState} from 'react';
 
 
 const Content = () => {
-    const handleNameChange = () => {
-        const names = ['Jason', 'Crystal', 'T-Rex'];
-        const int = Math.floor(Math.random() * 3);
-        return names[int];
-    }
+  const [name, setName] = useState('Jason');
+  const [count, setCount] = useState(0);
+  
+  const handleNameChange = () => {
+    const names = ['Jason', 'Crystal', 'T-Rex'];
+    const int = Math.floor(Math.random() * 3);
+    setName(names[int]);
+}
+
+  const handleClick = () => {
+    setCount(count + 1);
+    console.log(count);
+  }
+  const handleClick2 = () => {
+    console.log(count);
+  }
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  }
 
   return (
     <main>
         <p>
-          Hello {handleNameChange()}!
+          Hello {name}!
         </p>
+        <button onClick={handleNameChange}>Change Name</button>
+        <button onClick={handleClick}>Counter</button>
+        <button onClick={handleClick2}>Click Me!</button>
     </main>
   )
 }
