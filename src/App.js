5700868383abe25203ad64bf3f493dc3707acb1f
JMotDQS <jmotlagh@dqstaff.com>
2024-07-01 12:17:02 -0400

    Going through tutorial

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Jason';
+  const handleNameChange = () => {
+    const names = ['Jason', 'Crystal', 'T-Rex'];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to see what {name} changes.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello {handleNameChange()}!</p>
       </header>
     </div>
   );
