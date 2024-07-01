fdb35bb17aa5b22483e0071f9daae4c4ac269ed2
JMotDQS <jmotlagh@dqstaff.com>
2024-07-01 13:02:55 -0400

    More Tutorial Stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header.js';
+import Content from './Content.js';
+import Footer from './Footer.js'
 
 function App() {
-  const name = 'Jason';
-  const handleNameChange = () => {
-    const names = ['Jason', 'Crystal', 'T-Rex'];
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
-  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to see what {name} changes.
-        </p>
-        <p>Hello {handleNameChange()}!</p>
-      </header>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
